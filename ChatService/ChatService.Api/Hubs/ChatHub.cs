bc9708a86c46f1a679f7d3a58e5c33a59561e692
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2024-08-31 14:29:27 +0300

    add chathub

---
+++ ChatService/ChatService.Api/Hubs/ChatHub.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatService.Api.Hubs
+{
+    public class ChatHub : Hub
+    {
+
+
+        private static List<string> _rooms = new List<string>();
+
+        public async Task JoinRoom(string room)
+        {
+            // Eğer oda listede yoksa, listeye ekle
+            if (!_rooms.Contains(room))
+            {
+                _rooms.Add(room);
+            }
+            // Kullanıcıyı belirli bir odaya katılmak için gruba ekleyin
+            await Groups.AddToGroupAsync(Context.ConnectionId, room);
+
+            // Odaya katıldığını diğer kullanıcılara bildirmek için bir mesaj gönderebilirsiniz
+            await Clients.Group(room).SendAsync("ReceiveMessage", $"{Context.ConnectionId} odaya katıldı: {room}");
+
+            await Clients.Group("AdminGroup").SendAsync("ReceiveRoomList", _rooms);
+
+        }
+
+        public async Task AddAdminToGroup()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, "AdminGroup");
+
+            // Admin katıldığında mevcut oda listesini gönderin
+            await Clients.Caller.SendAsync("ReceiveRoomList", _rooms);
+        }
+
+
+
+        public async Task SendMessage(string roomName, string user, string message)
+        {
+            // Mesajı sadece belirtilen odaya gönder
+            await Clients.Group(roomName).SendAsync("ReceiveMessage", user, message);
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, Context.Items["RoomName"]?.ToString());
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
---
+++ ChatService/ChatService.Api/Program.cs
@@ -0,0 +1,42 @@
+using ChatService.Api.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigins",
+         builder =>
+         {
+             builder.WithOrigins("http://localhost:3000")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials();
+         });
+});
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddSignalR();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseCors("AllowSpecificOrigins");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapHub<ChatHub>("/chat");
+
+app.Run();
